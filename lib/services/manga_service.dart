d7a558f4dee349919ce0bfaf771975a452e9ac4a
Yerkhan <231132@astanait.edu.kz>
2025-02-14 15:15:05 +0500

    covers

--- lib/services/manga_service.dart
+++ lib/services/manga_service.dart
@@ -5,6 +5,9 @@ class MangaService {
   static Future<List<String>> loadMangaPages(String folder) async {
     final manifestContent = await rootBundle.loadString('AssetManifest.json');
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
-    return manifestMap.keys.where((path) => path.startsWith('assets/manga/$folder/') && path.endsWith('.jpg')).toList();
+    return manifestMap.keys
+        .where((path) => path.startsWith('assets/manga/$folder/') &&
+        (path.toLowerCase().endsWith('.jpg')))
+        .toList();
   }
 }
